06e141c6762f285dc5f7b522e3d0f587cc2cb826
mlatinov <latinovmetodi@gmail.com>
2025-06-17 22:19:12 +0300

    add eda and eda markdown

--- _targets.R
+++ _targets.R
@@ -8,6 +8,7 @@ library(tarchetypes)
 # Data Collection and Preparation
 library(tidyquant)
 library(tidyverse)
+library(timetk)
 library(wbstats)
 library(TTR)
 
@@ -16,11 +17,12 @@ library(modeltime)
 library(modeltime.ensemble)
 library(tidymodels)
 
+# Viz 
+library(DT)
+
 #### Source Functions 
 tar_source("functions/data_collection_functions.R")
-
-country = c("GB","US")
-fx_symbols <- c("EURUSD=X", "GBPUSD=X", "JPY=X", "CAD=X")
+tar_source("functions/data_eda_function_tk.R")
 
 #### Workflow #####
 
@@ -31,11 +33,25 @@ list(
     name = data_raw,
     command =
       get_data(
-        fx_symbols = c("EURUSD=X", "GBPUSD=X", "JPY=X", "CAD=X"),
-        from = 2000,
-        country = c("GB","US")
+        fx_symbols = c("EURUSD=X", "GBPUSD=X", "JPYUSD=X", "CADAUD=X"),
+        from = "2000-01-01",
+        country = c("GB","US","JP","CAD","AU")
         )
+    ),
+  
+  # EDA
+  tar_target(
+    name = diagnostics,
+    command = 
+      eda_tk(data = data_raw)
+  ),
+  
+  # EDA Report
+  tar_render(
+    eda_report,
+    path = "documents/eda_tk.Rmd",
+    params = list(
+      data = diagnostics
     )
-  
-  
+  )
 )
